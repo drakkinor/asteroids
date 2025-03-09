0533114af9c131a0fd66dc54e0fc1eae57c4353e
drakkinor <joseph.a.hughes@gmail.com>
2025-03-09 13:59:48 -0500

    player rotation with a and d

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000000)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = (clock.tick(60)/1000)
         #print(dt)
-
 
 
 if __name__ == "__main__":
